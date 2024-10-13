b170cbe21d5b8bac0bb0be516fea90cbf2a4fba8
Oplexitie <arthur.cottreau@gmail.com>
2024-10-13 20:16:55 +0200

    oops + cleanup

--- Scripts/Player/player_camera.gd
+++ Scripts/Player/player_camera.gd
@@ -36,22 +36,9 @@ func set_limits(left: int, right: int, top: int, bottom: int):
 	limit_bottom = bottom
 
 func handle_vertical_borders(delta: float):
-	var offset = ((abs(position.y - get_target_position().y)) / V_MARGIN)
+	var cam_offset = ((abs(position.y - get_target_position().y)) / V_MARGIN)
 	var not_grounded = 1 if !player.is_grounded() else 0
-	cam_drag_lerp = max(not_grounded, min(cam_drag_lerp, offset) - speed * delta)
+	cam_drag_lerp = max(not_grounded, min(cam_drag_lerp, cam_offset) - speed * delta)
 	
-	drag_top_margin = lerp(0.0, FULL_V_MARGIN / viewSize.y, cam_drag_lerp)	
+	drag_top_margin = lerp(0.0, FULL_V_MARGIN / viewSize.y, cam_drag_lerp)
 	drag_bottom_margin = drag_top_margin
-	
-#func _draw():
-	#var right = Vector2.RIGHT * drag_right_margin
-	#var left = Vector2.RIGHT * drag_left_margin
-	#var top_left = Vector2.UP * -drag_top_margin + left
-	#var top_right = Vector2.UP * -drag_top_margin + right
-	#var bottom_left = Vector2.DOWN * drag_bottom_margin + left
-	#var bottom_right = Vector2.DOWN * drag_bottom_margin + right
-	#draw_line(top_left, bottom_left, Color.WHITE)
-	#draw_line(top_right, bottom_right, Color.WHITE)
-	#draw_line(top_left, top_right, Color.WHITE)
-	#draw_line(bottom_left, bottom_right, Color.WHITE)
-	#draw_line(right, left, Color.WHITE)
